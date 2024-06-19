49f0ad1ff272a6ba95bd674be05b068808c03c56
Mike F <mfennemore@schubergphilis.com>
2024-06-19 13:45:52 +0200

    Adds action to enable

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Schuberg Philis'
 maintainer_email 'mdebruin@schubergphilis.com'
 license 'Apache-2.0'
 description 'Installs/Configures cis-hardening'
-version '0.1.16'
+version '0.1.17'
 chef_version '>= 16.0'
 
 depends 'os-hardening', '= 4.0.0'
